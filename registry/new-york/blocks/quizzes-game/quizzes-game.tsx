3711ed4e7446bdedd84dae25fb8b7c111753cfda
ducmai <duc.mai@icetea.io>
2025-10-21 15:20:51 +0700

    ui: answer

--- registry/new-york/blocks/quizzes-game/quizzes-game.tsx
+++ registry/new-york/blocks/quizzes-game/quizzes-game.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import { useState } from "react";
 import { Quiz } from "./type";
 import { MOCK_DATA } from "./constants";
+import { Button } from "../../ui/button";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  BookIcon,
+  CheckIcon,
+  RotateCcwIcon,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export const QuizzesGame = () => {
   const [quizzes, setQuizzes] = useState<Quiz[]>(MOCK_DATA);
@@ -8,34 +19,185 @@ export const QuizzesGame = () => {
   const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
   const [showExplanation, setShowExplanation] = useState(false);
 
-  const navigateQuiz = (direction: "next" | "prev") => {};
-
-  const prevQuiz = () => {
-    if (currentQuestionIndex <= 0) return;
-    navigateQuiz("prev");
-  };
-
-  const nextQuiz = () => {
-    if (currentQuestionIndex >= quizzes.length - 1) return;
-    navigateQuiz("next");
-  };
-
-  const isCorrectAnswer = () => {
-    if (selectedAnswer === null) return false;
-    return quizzes[currentQuestionIndex].correct_answer === selectedAnswer;
+  const navigateQuiz = (direction: "next" | "prev") => {
+    setSelectedAnswer(null);
+    setShowExplanation(false);
+    if (direction === "next") {
+      setCurrentQuestionIndex((prev) =>
+        prev < quizzes.length - 1 ? prev + 1 : prev
+      );
+      return;
+    }
+    setCurrentQuestionIndex((prev) => (prev > 0 ? prev - 1 : prev));
   };
 
   const handleSelectAnswer = (index: number) => {
     if (showExplanation) return;
     setSelectedAnswer(index);
-    setShowExplanation(true);
   };
 
-  return <div>Quizzes Game Component</div>;
+  return (
+    <div className="flex flex-col gap-y-4 w-full items-center p-4">
+      <Question question={quizzes[currentQuestionIndex].question} />
+
+      <div className="grid grid-cols-2 w-full gap-4">
+        {quizzes[currentQuestionIndex].choices.map((answer, index) => {
+          const isSelected = selectedAnswer === index;
+          const isCorrect =
+            index === quizzes[currentQuestionIndex].correct_answer &&
+            showExplanation;
+          const isIncorrect =
+            isSelected &&
+            index !== quizzes[currentQuestionIndex].correct_answer &&
+            showExplanation;
+          const type = isCorrect
+            ? "correct"
+            : isIncorrect
+            ? "incorrect"
+            : "neutral";
+
+          return (
+            <AnswerOption
+              key={answer}
+              answer={answer}
+              type={type}
+              isSelected={isSelected}
+              onClick={() => handleSelectAnswer(index)}
+            />
+          );
+        })}
+      </div>
+      <QuizControl
+        navigateQuiz={navigateQuiz}
+        quizIndex={currentQuestionIndex}
+        totalQuizzes={quizzes.length}
+        checkAnswer={() => setShowExplanation(true)}
+        resetQuiz={() => {
+          setSelectedAnswer(null);
+          setShowExplanation(false);
+        }}
+        isSelectedAnswer={selectedAnswer !== null}
+      />
+      {showExplanation && (
+        <Explanation explanation={quizzes[currentQuestionIndex].explanation} />
+      )}
+    </div>
+  );
 };
 
+const Question = ({ question }: { question: string }) => {
+  return (
+    <div className="flex w-full cursor-pointer flex-col gap-4 rounded-lg border-gray-300 border-2 border-b-4 bg-(--card) px-2 py-4 md:px-5 md:py-7">
+      <span className="whitespace-pre-line text-base md:text-lg">
+        {question}
+      </span>
+    </div>
+  );
+};
 
+const AnswerOption = ({
+  answer,
+  type,
+  isSelected,
+  onClick,
+}: {
+  answer: string;
+  type: "correct" | "incorrect" | "neutral";
+  isSelected: boolean;
+  onClick: () => void;
+}) => {
+  const baseClassname =
+    "border-2 border-gray-300 p-3 md:p-5 rounded-lg cursor-pointer";
 
-const AnswerOption = () => {
+  const getClassnameByType = () => {
+    switch (type) {
+      case "correct":
+        return cn(baseClassname, "border-green-500 bg-green-100");
+      case "incorrect":
+        return cn(baseClassname, "border-red-300 bg-red-100");
+      default:
+        if (isSelected) {
+          return cn(baseClassname, "border-gray-500 bg-gray-100");
+        }
+        return cn(baseClassname);
+    }
+  };
 
-}
+  return (
+    <div onClick={onClick} className={getClassnameByType()}>
+      {answer}
+    </div>
+  );
+};
+
+export const QuizControl = ({
+  navigateQuiz,
+  quizIndex,
+  totalQuizzes,
+  checkAnswer,
+  resetQuiz,
+  isSelectedAnswer,
+}: {
+  navigateQuiz: (direction: "next" | "prev") => void;
+  quizIndex: number;
+  totalQuizzes: number;
+  checkAnswer: () => void;
+  resetQuiz: () => void;
+  isSelectedAnswer: boolean;
+}) => {
+  return (
+    <div className="flex justify-between w-full items-center">
+      <div className="flex gap-x-2">
+        <Button
+          variant={"outline"}
+          onClick={() => navigateQuiz("prev")}
+          disabled={quizIndex === 0}
+          className="rounded-full size-9 border-gray-300 cursor-pointer"
+        >
+          <ArrowLeftIcon className="size-4" />
+        </Button>
+        <Button
+          variant={"outline"}
+          onClick={() => navigateQuiz("next")}
+          disabled={quizIndex === totalQuizzes - 1}
+          className="rounded-full size-9 border-gray-300 cursor-pointer"
+        >
+          <ArrowRightIcon className="size-4" />
+        </Button>
+      </div>
+
+      <div className="flex gap-x-2 items-center">
+        <Button
+          onClick={resetQuiz}
+          disabled={!isSelectedAnswer}
+          className="rounded-full size-9 border-gray-300 cursor-pointer"
+        >
+          <RotateCcwIcon className="size-4" />
+        </Button>
+        <Button
+          onClick={checkAnswer}
+          disabled={!isSelectedAnswer}
+          className="rounded-xl cursor-pointer"
+        >
+          Kiểm tra
+          <CheckIcon className="size-4" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const Explanation = ({ explanation }: { explanation: string }) => {
+  return (
+    <div className="flex gap-3 rounded-xl bg-[#D9E2FF4D] p-4 w-full mt-2">
+      <div className="w-[3px] flex-shrink-0 self-stretch rounded-full bg-[#0C66E4]" />
+      <div className="flex flex-1 flex-col gap-2">
+        <div className="flex items-center gap-2">
+          <BookIcon className="size-5" />
+          <p>Giải thích</p>
+        </div>
+        <p className="whitespace-pre-line text-sm">{explanation}</p>
+      </div>
+    </div>
+  );
+};
